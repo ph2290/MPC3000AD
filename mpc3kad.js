cd7a852658dd26178618b37dad2dc3310468093b
ph2290 <peterhumphrey@gmail.com>
2024-10-18 12:52:23 +0100

    Update mpc3kad.js

--- mpc3kad.js
+++ mpc3kad.js
@@ -1,77 +1,35 @@
-// create web audio api context
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 $("td div").on("mousedown touchstart", function (e) {
   e.stopPropagation();
   e.preventDefault();
   var i = parseInt($(this).parent().text()) - 1;
-  //var sdfg = $("audio").eq(1);
+  var audioElements = $("audio");
 
-  document.getElementsByTagName("audio")[i].currentTime = 0;
-
-  document.getElementsByTagName("audio")[i].play();
-
-  //sdfg.play()
-
-  $(this).css("background", "transparent");
+  if (i >= 0 && i < audioElements.length) {
+    var audio = audioElements.eq(i).get(0);
+    audio.currentTime = 0;
+    audio.play();
+    $(this).css("background", "transparent");
+  }
 });
+
 $("td div").on("mouseup touchend", function () {
   $(this).css("background", "transparent");
 });
 
-// for each file input...
-
 $("input").each(function (index, element) {
-  var i = index;
   $(this).on("change", function (e) {
     var target = e.currentTarget;
     var file = target.files[0];
-    var reader = new FileReader();
-
-    // document.getElementsByTagName('audio')[i].currentTime = 0;
-    //  console.log($audio[0]);
-    if (target.files && file) {
+    
+    if (file) {
       var reader = new FileReader();
       reader.onload = function (e) {
-        document.getElementsByTagName("audio")[i].src = e.target.result;
-        //document.getElementsByTagName('audio')[0].play();
-        $("td div").eq(i).css("background", "transparent");
+        $("audio").eq(index).attr("src", e.target.result);
+        $("td div").eq(index).css("background", "transparent");
       };
       reader.readAsDataURL(file);
     }
   });
 });
-
-function handleDrop(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  console.log(e);
-
-  var sdfg = e.target;
-  var inum = parseInt($(this).parent().text()) - 1;
-  var files = e.dataTransfer.files;
-  //for (var i = 0, f; f = files[i]; i++) {
-
-  document.getElementsByTagName("input")[inum].files = files;
-
-  //  console.log(f)
-  //  filein.files = files;
-
-  var event = document.createEvent("UIEvents");
-  event.initUIEvent("change", true, true);
-  document.getElementsByTagName("input")[inum].dispatchEvent(event);
-  // Read the File objects in this FileList.
-  //  }
-}
-
-var allTags = document.getElementsByTagName("div");
-for (var i = 0, len = allTags.length; i < len; i++) {
-  // Setup the dnd listeners.
-  var dropZone = document.getElementsByTagName("div")[i];
-  // dropZone.addEventListener('dragover', handleDragOver, false);
-  dropZone.addEventListener("drop", handleDrop);
-
-  dropZone.ondragover = dropZone.ondragenter = function (evt) {
-    evt.preventDefault();
-  };
-}
